5c3a75048b25eda19f753e859cc2ba9e20df8129
MarcosPCB <marcospaulocbrito@gmail.com>
2024-10-17 00:06:32 -0300

    Job: -Added new route /message for sending a single message to the Broker's queue -Added a test for the new route and a test for checking error throw for GenerateMessages

--- job/src/modules/messager/Controller.ts
+++ job/src/modules/messager/Controller.ts
@@ -2,6 +2,7 @@ import { z } from "zod";
 import makeGenerateMessages from "./services/GenerateMessages";
 import { Request, Response } from "express";
 import handleError from "../../shared/errors/handleError";
+import makeSendSingleMessage from "./services/SendSingleMessage";
 
 export default class Controller {
     async generateMessages(req: Request, res: Response) {
@@ -16,6 +17,29 @@ export default class Controller {
 
             const sent = await service.execute(queue);
             res.status(200).send(`Sent ${sent} sellers to broker`);
+        } catch(err: any) {
+            const { status, message, data } = handleError(err);
+            return res.status(status).json({ message, data });
+        }
+    }
+
+    async sendSingleMessage(req: Request, res: Response) {
+        try {
+            const params = z.object({
+                seller: z.object({
+                    nome: z.string(),
+                    telefone: z.string(),
+                    id: z.number()
+                }),
+                queue: z.string().optional()
+            });
+
+            const { seller, queue } = params.parse(req.body);
+
+            const service = makeSendSingleMessage();
+
+            const sent = await service.execute(seller, queue);
+            res.status(200).send(`success`);
         } catch(err: any) {
             const { status, message, data } = handleError(err);
             return res.status(status).json({ message, data });
